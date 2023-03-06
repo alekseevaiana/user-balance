efc77c54b7a67bc526bfbca755c11b1358d01970
alekseevayana <alekseevayana@bk.ru>
2023-03-06 09:14:52 -0800

    Add input and button

--- web/src/pages/UserBalancePage.tsx
+++ web/src/pages/UserBalancePage.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+
 import UserBalanceCell from 'src/components/UserBalanceCell'
 
 export default function UserBalancePage() {
-  return <UserBalanceCell id="ethers.eth" />
+  const [id, setId] = useState('')
+  const [value, setValue] = useState('ethers.eth')
+  // additional case 0xF02c1c8e6114b1Dbe8937a39260b5b0a374432bB
+
+  const checkBalance = () => {
+    setId(value)
+  }
+
+  return (
+    <div>
+      <TextField
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        size="small"
+      />
+      <Button variant="contained" onClick={checkBalance}>
+        Check balance
+      </Button>
+
+      {id ? <UserBalanceCell id={id} /> : ''}
+    </div>
+  )
 }
